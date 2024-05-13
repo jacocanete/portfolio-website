5aa9adfb54d785bef45e553b78d9e192fbbd3d05
jacocanete <jacogagarincanete@gmail.com>
2024-05-13 20:28:40 +0800

    chore: Update experiencesData with correct date range in data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -62,15 +62,7 @@ export const experiencesData = [
     description:
       "I was never to become a full-time web developer, but I was offered a position as a front-end developer. I worked on a variety of projects, gaining experience with HTML, CSS, Javascript, and PHP. I also learned how to work with APIs and integrate them into web applications. This experience solidified my decision to pursue web development as a career. I took Figma designs and turned them into fully functional websites.",
     icon: React.createElement(FaWordpressSimple),
-    date: "2023 - 2024",
-  },
-  {
-    title: "Web Developer",
-    location: "Remote",
-    description:
-      "I had the opportunity to work as a web designer and developer for a PPC Agency. I was responsible for designing and developing websites and funnels for my boss. This experience gave me a solid foundation in web design and development.",
-    icon: React.createElement(FaWordpressSimple),
-    date: "2023 - 2024",
+    date: "2022 - 2024",
   },
 ] as const;
 
