df9b4377d9e5ba46982b4480c0ee99778f21503d
jacocanete <jacogagarincanete@gmail.com>
2024-05-20 20:03:34 +0800

    fix: Updated Download CV href property

--- components/intro.tsx
+++ components/intro.tsx
@@ -88,7 +88,7 @@ export default function Intro() {
         </Link>
         <a
           className="bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition group cursor-pointer border border-black/10 dark:bg-white/10"
-          href="/Canete_Resume.pdf"
+          href="/CV.pdf"
           download
         >
           Download CV{" "}
